8407d9011f1bcdaa684a113c0e7a4fa62d14e914
Gabe Hollombe <gabe@avantbard.com>
2018-08-07 20:28:15 +1000

    added connect component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
 import React, { Component } from 'react';
 
-import Amplify from 'aws-amplify';
+import Amplify, { graphqlOperation }  from "aws-amplify";
+import { Connect, withAuthenticator } from 'aws-amplify-react'; 
+
 import aws_exports from './aws-exports';
 
-import { withAuthenticator } from 'aws-amplify-react'; 
 
 
 Amplify.configure(aws_exports);
 
 class App extends Component {
   render() {
-    return (
+    const ListView = ({ events }) => (
       <div>
-        Hello World
+          <h3>All events</h3>
+          <ul>
+              {events.map(event => <li key={event.id}>{event.name} ({event.id})</li>)}
+          </ul>
       </div>
-    );
+  );
+
+  const ListEvents = `query ListEvents {
+      listEvents {
+          items {
+            id
+            name
+          }
+      }
+  }`;
+
+  return (
+      <Connect query={graphqlOperation(ListEvents)}>
+          {({ data, loading, errors }) => {
+            if (loading) return <div>Loading...</div>;
+            if (errors.length > 0) return <div>{JSON.stringify(errors)}</div>;
+            if (!data.listEvents) return;
+            return <ListView events={data.listEvents.items} />;
+          }}
+      </Connect>
+  )
   }
 }
 
