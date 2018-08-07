fa47fac1037084849734749b05b12c88cc8db989
Gabe Hollombe <gabe@avantbard.com>
2018-08-07 20:34:54 +1000

    add create event hardcoded data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Amplify, { graphqlOperation }  from "aws-amplify";
+import Amplify, { API, graphqlOperation }  from "aws-amplify";
 import { Connect, withAuthenticator } from 'aws-amplify-react'; 
 
 import aws_exports from './aws-exports';
@@ -8,6 +8,41 @@ import aws_exports from './aws-exports';
 
 
 Amplify.configure(aws_exports);
+
+class EventForm extends Component {
+  createEvent = async (e) => {
+    e.preventDefault();
+
+    const CreateEvent = `mutation CreateEvent($name: String!, $when: String!, $where: String!, $description: String!) {
+      createEvent(name: $name, when: $when, where: $where, description: $description) {
+        id
+        name
+        where
+        when
+        description
+      }
+    }`;
+    
+    // Mutation
+    const eventDetails = {
+        name: 'Party tonight!',
+        when: '8:00pm',
+        where: 'Ballroom',
+        description: 'Coming together as a team!'
+    };
+    
+    const newEvent = await API.graphql(graphqlOperation(CreateEvent, eventDetails));
+    console.log(newEvent);
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.createEvent}>Create Event</button>
+      </div>
+    )
+  }
+}
 
 class App extends Component {
   render() {
@@ -30,14 +65,20 @@ class App extends Component {
   }`;
 
   return (
-      <Connect query={graphqlOperation(ListEvents)}>
-          {({ data, loading, errors }) => {
-            if (loading) return <div>Loading...</div>;
-            if (errors.length > 0) return <div>{JSON.stringify(errors)}</div>;
-            if (!data.listEvents) return;
-            return <ListView events={data.listEvents.items} />;
-          }}
-      </Connect>
+      <div>
+        <EventForm />
+
+        <Connect query={graphqlOperation(ListEvents)}>
+            {
+              ({ data, loading, errors }) => {
+                if (loading) return <div>Loading...</div>;
+                if (errors.length > 0) return <div>{JSON.stringify(errors)}</div>;
+                if (!data.listEvents) return;
+                return <ListView events={data.listEvents.items} />;
+              }
+            }
+        </Connect>
+      </div>
   )
   }
 }
